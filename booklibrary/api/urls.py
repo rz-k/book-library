4ac92c42c9e6e246db1d363462cfa89f04483912
root <adamak.tnh@gmail.com>
2024-07-21 20:20:51 +0330

    added book and review in django admin panel

--- booklibrary/api/urls.py
+++ booklibrary/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('users/', include(('booklibrary.users.urls', 'users'))),
-    path('authentication/', include(('booklibrary.authentication.urls', 'authentication')))
+    path('authentication/', include(('booklibrary.authentication.urls', 'authentication'))),
+    path('book/', include(('booklibrary.book.urls', 'book'))),
 ]
--- booklibrary/book/admin.py
+++ booklibrary/book/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from booklibrary.book.models import Book, Reviews
 
+
+admin.site.register(Book)
+admin.site.register(Reviews)
 # Register your models here.
