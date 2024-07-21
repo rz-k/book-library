f6bfa98f51f1efceea7bbbf9ef99c7e2544049fb
root <adamak.tnh@gmail.com>
2024-07-21 17:07:01 +0330

    added book && rating madel

--- booklibrary/book/models.py
+++ booklibrary/book/models.py
@@ -1,3 +1,50 @@
+from booklibrary.common.models import BaseModel
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+
+class Book(BaseModel):
+    title = models.CharField(
+        max_length=200, 
+        unique=True,
+        db_index=True
+    )
+
+    author = models.CharField(
+        max_length=200
+    )
+
+    genre = models.CharField(
+        max_length=50,
+        db_index=True
+    )
+
+    def __str__(self) -> str:
+        return self.title[:30]
+
+class Reviews(BaseModel):
+
+    book = models.ForeignKey(
+        to=Book,
+        on_delete=models.CASCADE,
+        related_name="book_reviews",
+    )
+
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name="user_reviews",
+    )
+
+    rating = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
+
+    def __str__(self) -> str:
+        return f"{self.rating}:{self.user.username}-{self.book.title[:10]}"
